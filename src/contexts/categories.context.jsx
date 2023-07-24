5e57f7bb7bf09b91ba235cbbdd3b5c8cca4a09da
Shehan Maxwell <shehanbbn@gmail.com>
2023-07-24 21:56:08 +0530

    modified 3 files

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -24,11 +24,20 @@ export const CategoriesProvider = ({ children }) => {
   const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  console.log('loading', loading);
+  useEffect(() => {
+    if (data) {
+      const { collections } = data;
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
 
-  console.log('data', data);
-
-  const value = { categoriesMap };
+      setCategoriesMap(collectionsMap);
+    }
+  }, [data]);
+ 
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
